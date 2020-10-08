904c0f7206f42e40a5ebd7d2ecc6cab5743a648d
Json Carmock <42975288+jsonCarmock@users.noreply.github.com>
2020-10-08 09:32:56 +0200

    Control count of unit test iterations with env var (#2)

--- makefiles/test-unit.mk
+++ makefiles/test-unit.mk
@@ -1,8 +1,9 @@
 GO ?= go
+UNIT_TEST_COUNT ?= 5
 
 ## Run unit tests
 test-unit:
 	@echo "Running unit tests."
-	@$(GO) test -gcflags=-l -coverprofile=unit.coverprofile -count 5 -covermode=atomic -race ./...
+	@$(GO) test -gcflags=-l -coverprofile=unit.coverprofile -count $(UNIT_TEST_COUNT) -covermode=atomic -race ./...
 
 .PHONY: test-unit
