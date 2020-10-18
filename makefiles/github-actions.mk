4b8efd1624ade42f1249bb30bb329800f4598288
Viacheslav Poturaev <vearutop@gmail.com>
2020-10-18 10:51:59 +0200

    Remove older Go versions from test

--- makefiles/github-actions.mk
+++ makefiles/github-actions.mk
@@ -2,6 +2,6 @@ GO ?= go
 
 ## Replace GitHub Actions from template
 github-actions:
-	@rm -rf $(PWD)/.github && cp -r $(DEVGO_PATH)/templates/.github $(PWD)/ && rm -f $(PWD)/.github/workflows/doc.go && git add $(PWD)/.github
+	@mkdir -p $(PWD)/.github/workflows && cp $(DEVGO_PATH)/templates/.github/workflows/*.yml $(PWD)/.github/workflows/ && git add $(PWD)/.github
 
 .PHONY: github-actions
