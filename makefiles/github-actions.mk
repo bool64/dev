7879ec23ac5c689748cdda43a986893383a1d4ff
Viacheslav Poturaev <vearutop@gmail.com>
2020-10-18 10:59:13 +0200

    Update make github-actions

--- makefiles/github-actions.mk
+++ makefiles/github-actions.mk
@@ -2,6 +2,6 @@ GO ?= go
 
 ## Replace GitHub Actions from template
 github-actions:
-Update 	@mkdir -p $(PWD)/.github/workflows && chmod +w $(PWD)/.github/workflows/*.yml && cp $(DEVGO_PATH)/templates/.github/workflows/*.yml $(PWD)/.github/workflows/ && chmod +w $(PWD)/.github/workflows/*.yml && git add $(PWD)/.github
+	@mkdir -p $(PWD)/.github/workflows && chmod +w $(PWD)/.github/workflows/*.yml && cp $(DEVGO_PATH)/templates/.github/workflows/*.yml $(PWD)/.github/workflows/ && chmod +w $(PWD)/.github/workflows/*.yml && git add $(PWD)/.github
 
 .PHONY: github-actions
