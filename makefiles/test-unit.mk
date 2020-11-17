c4974d735c49ded083b7a70f5b2c6c7f78a44e6e
Viacheslav Poturaev <vearutop@gmail.com>
2020-11-18 00:20:40 +0100

    Upgrade golangci-lint

--- makefiles/test-unit.mk
+++ makefiles/test-unit.mk
@@ -4,6 +4,11 @@ UNIT_TEST_COUNT ?= 5
 ## Run unit tests
 test-unit:
 	@echo "Running unit tests."
+	@$(GO) test -gcflags=-l -coverprofile=unit.coverprofile -covermode=atomic -race ./...
+
+## Run unit tests multiple times
+test-unit-multi:
+	@echo "Running unit tests ${UNIT_TEST_COUNT} times."
 	@$(GO) test -gcflags=-l -coverprofile=unit.coverprofile -count $(UNIT_TEST_COUNT) -covermode=atomic -race ./...
 
-.PHONY: test-unit
+.PHONY: test-unit test-unit-multi
