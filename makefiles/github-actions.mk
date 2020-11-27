e376929f4cd834195d8edaeb07462fa616e56552
Viacheslav Poturaev <vearutop@gmail.com>
2020-11-27 10:56:36 +0100

    Add benchmark task

--- makefiles/github-actions.mk
+++ makefiles/github-actions.mk
@@ -4,7 +4,8 @@ GO ?= go
 github-actions:
 	@mkdir -p $(PWD)/.github/workflows && chmod +w $(PWD)/.github/workflows/*.yml \
 		&& cp $(DEVGO_PATH)/templates/github/workflows/*.yml $(PWD)/.github/workflows/ \
-		&& ([ -z "$BENCH_COUNT" ] && rm $(PWD)/.github/workflows/bench.yml) \
-		&& chmod +w $(PWD)/.github/workflows/*.yml && git add $(PWD)/.github
+		&& chmod +w $(PWD)/.github/workflows/*.yml && git add $(PWD)/.github/workflows \
+		&& ([ -z "$BENCH_COUNT" ] && rm $(PWD)/.github/workflows/bench.yml)
+
 
 .PHONY: github-actions
