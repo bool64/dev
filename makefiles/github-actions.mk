d911ec78751e429be377fac84763d580d59b77a7
Viacheslav Poturaev <vearutop@gmail.com>
2020-11-27 17:45:18 +0100

    Do not fail on initial github actions setup

--- makefiles/github-actions.mk
+++ makefiles/github-actions.mk
@@ -2,7 +2,7 @@ GO ?= go
 
 ## Replace GitHub Actions from template
 github-actions:
-	@mkdir -p $(PWD)/.github/workflows && chmod +w $(PWD)/.github/workflows/*.yml \
+	@mkdir -p $(PWD)/.github/workflows && (chmod +w $(PWD)/.github/workflows/*.yml || echo "could not chmod +w existing workflows") \
 		&& cp $(DEVGO_PATH)/templates/github/workflows/*.yml $(PWD)/.github/workflows/ \
 		&& chmod +w $(PWD)/.github/workflows/*.yml && git add $(PWD)/.github/workflows \
 		&& (test -z "$(BENCH_COUNT)" && rm $(PWD)/.github/workflows/bench.yml || echo "keeping bench.yml")
