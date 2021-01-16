38f003eafde01c329476e74bcf26ecb856b03312
Viacheslav Poturaev <vearutop@gmail.com>
2021-01-16 23:51:55 +0100

    Add version flags and build target

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -1,21 +1,21 @@
 GO ?= go
-BUILD_LDFLAGS ?= ""
-BUILD_PKG ?= .
-BUILD_DIR ?= ./bin
+BUILD_LDFLAGS ?= "" # Override in app Makefile to add custom ldflags, example BUILD_LDFLAGS="-s -w"
+BUILD_PKG ?= . # Override in app Makefile to control build target, example BUILD_PKG=./cmd/my-app
+BUILD_DIR ?= ./bin # Override in app Makefile to control build artifact destination.
 
 ## Build Linux binary
 build-linux:
 	@echo "Building Linux AMD64 binary, GOFLAGS: $(GOFLAGS)"
-	@GOOS=linux GOARCH=amd64 $(GO) build -ldflags "$(BUILD_LDFLAGS) $(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@GOOS=linux GOARCH=amd64 $(GO) build -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build binary
 build:
 	@echo "Building binary, GOFLAGS: $(GOFLAGS)"
-	@$(GO) build -ldflags "$(BUILD_LDFLAGS) $(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@$(GO) build -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build and run binary
 run:
 	@echo "Running binary"
-	@$(GO) run -ldflags "$(BUILD_LDFLAGS) $(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" $(BUILD_PKG)
+	@$(GO) run -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" $(BUILD_PKG)
 
 .PHONY: build-linux build run
