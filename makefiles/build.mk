b329809de26e791df53c257e11ca6e1d048c60e2
Viacheslav Poturaev <vearutop@gmail.com>
2021-01-16 23:43:50 +0100

    Add version flags and build target

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -6,16 +6,16 @@ BUILD_DIR ?= ./bin
 ## Build Linux binary
 build-linux:
 	@echo "Building Linux AMD64 binary, GOFLAGS: $(GOFLAGS)"
-	@GOOS=linux GOARCH=amd64 $(GO) build -ldflags "$(BUILD_LDFLAGS)" -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@GOOS=linux GOARCH=amd64 $(GO) build -ldflags "$(BUILD_LDFLAGS) $(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build binary
 build:
 	@echo "Building binary, GOFLAGS: $(GOFLAGS)"
-	@$(GO) build -ldflags "$(BUILD_LDFLAGS)" -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@$(GO) build -ldflags "$(BUILD_LDFLAGS) $(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build and run binary
 run:
 	@echo "Running binary"
-	@$(GO) run -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" $(BUILD_PKG)
+	@$(GO) run -ldflags "$(BUILD_LDFLAGS) $(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" $(BUILD_PKG)
 
 .PHONY: build-linux build run
