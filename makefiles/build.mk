6870f4354b455d215bd424eabc1b9c21bce12e4f
Viacheslav Poturaev <vearutop@gmail.com>
2021-01-16 23:31:01 +0100

    Add version flags and build target

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -6,12 +6,12 @@ BUILD_DIR ?= ./bin
 ## Build Linux binary
 build-linux:
 	@echo "Building Linux AMD64 binary, GOFLAGS: $(GOFLAGS)"
-	@GOOS=linux GOARCH=amd64 $(GO) build -ldflags "$(BUILD_LDFLAGS)$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@GOOS=linux GOARCH=amd64 $(GO) build -ldflags "$(BUILD_LDFLAGS)" -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build binary
 build:
 	@echo "Building binary, GOFLAGS: $(GOFLAGS)"
-	@$(GO) build -ldflags "$(BUILD_LDFLAGS)$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@$(GO) build -ldflags "$(BUILD_LDFLAGS)" -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh)" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build and run binary
 run:
