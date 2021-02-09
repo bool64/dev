df91e21347c5c7531703421bac8966f50a839015
Viacheslav Poturaev <vearutop@gmail.com>
2021-02-10 00:07:48 +0100

    Update default golangci-lint version

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ import _ "github.com/bool64/dev" // Include development helpers to project.
 Add `Makefile` to your module with includes standard targets.
 
 ```Makefile
-GOLANGCI_LINT_VERSION := "v1.35.2" # Optional.
+GOLANGCI_LINT_VERSION := "v1.36.0" # Optional.
 
 # The head of Makefile determines location of dev-go to include standard targets.
 GO ?= go
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ -z "$GO" ] && GO=go
-[ -z "$GOLANGCI_LINT_VERSION" ] && GOLANGCI_LINT_VERSION="v1.35.2"
+[ -z "$GOLANGCI_LINT_VERSION" ] && GOLANGCI_LINT_VERSION="v1.36.0"
 
 # detecting GOPATH and removing trailing "/" if any
 GOPATH="$(go env GOPATH)"
@@ -11,9 +11,9 @@ GOPATH=${GOPATH%/}
 [[ ":$PATH:" != *"$GOPATH/bin"* ]] && PATH=$PATH:"$GOPATH"/bin
 
 # checking if golangci-lint is available
-if ! command -v golangci-lint-"$GOLANGCI_LINT_VERSION" > /dev/null ; then
-    echo "Installing golangci-lint $GOLANGCI_LINT_VERSION...";
-    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /tmp "$GOLANGCI_LINT_VERSION" && mv /tmp/golangci-lint "$GOPATH"/bin/golangci-lint-"$GOLANGCI_LINT_VERSION"
+if ! command -v golangci-lint-"$GOLANGCI_LINT_VERSION" >/dev/null; then
+  echo "Installing golangci-lint $GOLANGCI_LINT_VERSION..."
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /tmp "$GOLANGCI_LINT_VERSION" && mv /tmp/golangci-lint "$GOPATH"/bin/golangci-lint-"$GOLANGCI_LINT_VERSION"
 fi
 
 this_path=$(dirname "$0")
@@ -27,6 +27,6 @@ echo "Checking packages."
 golangci-lint-"$GOLANGCI_LINT_VERSION" run -c "$golangci_yml" ./... || exit 1
 
 if [[ -d "./internal" && -d "./cmd" ]]; then
-    echo "Checking unused exported symbols in internal packages."
-    golangci-lint-"$GOLANGCI_LINT_VERSION" run -c "$this_path"/.golangci-internal.yml ./internal/... ./cmd/... || exit 1
+  echo "Checking unused exported symbols in internal packages."
+  golangci-lint-"$GOLANGCI_LINT_VERSION" run -c "$this_path"/.golangci-internal.yml ./internal/... ./cmd/... || exit 1
 fi
