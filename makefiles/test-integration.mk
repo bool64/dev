e7efa7e29fe9eb0c294b5db395b240d4ab778e87
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2021-05-22 11:05:55 +0200

    Add task and makefile target for integration test (#19)

---
+++ makefiles/test-integration.mk
@@ -0,0 +1,21 @@
+GO ?= go
+
+# Override in app Makefile to add custom ldflags, example BUILD_LDFLAGS="-s -w"
+BUILD_LDFLAGS ?= ""
+INTEGRATION_TEST_TARGET ?= -coverpkg ./internal/... integration_test.go
+
+## Run integration tests
+test-integration:
+	@make start-deps
+	@echo "Running integration tests."
+	@$(GO) test -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -race -cover -coverprofile ./integration.coverprofile $(INTEGRATION_TEST_TARGET)
+
+## Start dependencies for integration tests or local dev via docker-compose up
+start-deps:
+	@test ! -f ./docker-compose.integration-test.yml || docker-compose -p "$(shell basename $$PWD)" -f ./docker-compose.integration-test.yml up -d
+
+## Stop dependencies for integration tests or local dev via docker-compose down
+stop-deps:
+	@test ! -f ./docker-compose.integration-test.yml || docker-compose -p "$(shell basename $$PWD)" -f ./docker-compose.integration-test.yml down
+
+.PHONY: test-integration start-deps stop-deps
