a1edf5a68112db44b1cb5e1742f6f8901b3930dc
Viacheslav Poturaev <vearutop@gmail.com>
2021-05-22 10:19:34 +0200

    Add task and makefile target for integration test

--- makefiles/github-actions.mk
+++ makefiles/github-actions.mk
@@ -6,6 +6,7 @@ github-actions:
 		&& cp $(DEVGO_PATH)/templates/github/workflows/*.yml $(PWD)/.github/workflows/ \
 		&& chmod +w $(PWD)/.github/workflows/*.yml && git add $(PWD)/.github/workflows \
 		&& (test -z "$(BENCH_COUNT)" && rm $(PWD)/.github/workflows/bench.yml || echo "keeping bench.yml") \
-		&& (test -z "$(RELEASE_TARGETS)" && rm $(PWD)/.github/workflows/release-assets.yml || echo "keeping release-assets.yml")
+		&& (test -z "$(RELEASE_TARGETS)" && rm $(PWD)/.github/workflows/release-assets.yml || echo "keeping release-assets.yml") \
+		&& (test -z "$(INTEGRATION_TEST_TARGET)" && rm $(PWD)/.github/workflows/test-integration.yml || echo "keeping test-integration.yml")
 
 .PHONY: github-actions
