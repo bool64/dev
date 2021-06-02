c545d368843a2b6de649039ab5cf9d7c2080e61b
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2021-06-02 23:44:26 +0200

    Update golangci-lint, fix test targets with CGO_ENABLED=0 (#21)

--- makefiles/test-unit.mk
+++ makefiles/test-unit.mk
@@ -4,11 +4,11 @@ UNIT_TEST_COUNT ?= 2
 ## Run unit tests
 test-unit:
 	@echo "Running unit tests."
-	@$(GO) test -short -gcflags=-l -coverprofile=unit.coverprofile -covermode=atomic -race ./...
+	@CGO_ENABLED=1 $(GO) test -short -gcflags=-l -coverprofile=unit.coverprofile -covermode=atomic -race ./...
 
 ## Run unit tests multiple times
 test-unit-multi:
 	@echo "Running unit tests ${UNIT_TEST_COUNT} times."
-	@$(GO) test -short -gcflags=-l -coverprofile=unit.coverprofile -count $(UNIT_TEST_COUNT) -covermode=atomic -race ./...
+	@CGO_ENABLED=1 $(GO) test -short -gcflags=-l -coverprofile=unit.coverprofile -count $(UNIT_TEST_COUNT) -covermode=atomic -race ./...
 
 .PHONY: test-unit test-unit-multi
