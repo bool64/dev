ddda16d69aebe9cabbab4675179744d729a54c00
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2021-08-24 12:58:42 +0200

    Separate benchmark diff from benchmark result (#24)

--- makefiles/bench.mk
+++ makefiles/bench.mk
@@ -4,15 +4,20 @@ MASTER_BRANCH ?= master
 REF_NAME ?= $(shell git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
 
 ## Run benchmark and show result stats, iterations count controlled by BENCH_COUNT, default 5.
-bench: bench-run bench-stat
+bench: bench-run bench-stat-diff bench-stat
 
 ## Run benchmark, iterations count controlled by BENCH_COUNT, default 5.
 bench-run:
-	@$(GO) test -bench=. -count=$(BENCH_COUNT) -run=^a  ./... >bench-$(REF_NAME).txt || (cat bench-$(REF_NAME).txt && exit 1)
+	@$(GO) test -bench=. -count=$(BENCH_COUNT) -run=^a  ./... | tee bench-$(REF_NAME).txt || (cat bench-$(REF_NAME).txt && exit 1)
+
+## Show benchmark comparison with base branch.
+bench-stat-diff:
+	@test -s $(GOPATH)/bin/benchstat || GO111MODULE=off GOFLAGS= GOBIN=$(GOPATH)/bin $(GO) get -u golang.org/x/perf/cmd/benchstat
+	@test -e bench-$(MASTER_BRANCH).txt && benchstat bench-$(MASTER_BRANCH).txt bench-$(REF_NAME).txt
 
 ## Show result of benchmark.
 bench-stat:
 	@test -s $(GOPATH)/bin/benchstat || GO111MODULE=off GOFLAGS= GOBIN=$(GOPATH)/bin $(GO) get -u golang.org/x/perf/cmd/benchstat
-	@test -e bench-$(MASTER_BRANCH).txt && benchstat bench-$(MASTER_BRANCH).txt bench-$(REF_NAME).txt || $(GOPATH)/bin/benchstat bench-$(REF_NAME).txt
+	@$(GOPATH)/bin/benchstat bench-$(REF_NAME).txt
 
-.PHONY: bench bench-run bench-stat
+.PHONY: bench bench-run bench-stat-diff bench-stat
