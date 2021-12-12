878fd53228da34af926e1faa76fedd86e55bf207
Viacheslav Poturaev <vearutop@gmail.com>
2021-12-12 09:59:53 +0100

    Fix benchstat installation

--- makefiles/bench.mk
+++ makefiles/bench.mk
@@ -7,18 +7,19 @@ SHELL := /bin/bash
 ## Run benchmark and show result stats, iterations count controlled by BENCH_COUNT, default 5.
 bench: bench-run bench-stat-diff bench-stat
 
+bench-stat-cli:
+	@test -s $(GOPATH)/bin/benchstat || GOFLAGS= GOBIN=$(GOPATH)/bin $(GO) install golang.org/x/perf/cmd/benchstat@latest
+
 ## Run benchmark, iterations count controlled by BENCH_COUNT, default 5.
 bench-run:
 	@set -o pipefail && $(GO) test -bench=. -count=$(BENCH_COUNT) -run=^a  ./... | tee bench-$(REF_NAME).txt
 
 ## Show benchmark comparison with base branch.
-bench-stat-diff:
-	@test -s $(GOPATH)/bin/benchstat || GO111MODULE=off GOFLAGS= GOBIN=$(GOPATH)/bin $(GO) install golang.org/x/perf/cmd/benchstat@latest
+bench-stat-diff: bench-stat-cli
 	@test ! -e bench-$(MASTER_BRANCH).txt || benchstat bench-$(MASTER_BRANCH).txt bench-$(REF_NAME).txt
 
 ## Show result of benchmark.
-bench-stat:
-	@test -s $(GOPATH)/bin/benchstat || GO111MODULE=off GOFLAGS= GOBIN=$(GOPATH)/bin $(GO) install golang.org/x/perf/cmd/benchstat@latest
+bench-stat: bench-stat-cli
 	@$(GOPATH)/bin/benchstat bench-$(REF_NAME).txt
 
 .PHONY: bench bench-run bench-stat-diff bench-stat
