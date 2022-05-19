2796a83c0f874673044e3d740e211579ba9fdd23
Viacheslav Poturaev <vearutop@gmail.com>
2022-05-19 10:09:10 +0200

    Update golangci-lint to v1.46.2

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ -z "$GO" ] && GO=go
-[ -z "$GOLANGCI_LINT_VERSION" ] && GOLANGCI_LINT_VERSION="v1.45.2"
+[ -z "$GOLANGCI_LINT_VERSION" ] && GOLANGCI_LINT_VERSION="v1.46.2"
 
 # detecting GOPATH and removing trailing "/" if any
 GOPATH="$(go env GOPATH)"
