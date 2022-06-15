ce9dc5801af873f34bb33187a1e7ec9c7b914078
Viacheslav Poturaev <vearutop@gmail.com>
2022-06-15 10:09:28 +0200

    Trim paths from a built artifact (#41)

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -12,12 +12,12 @@ BUILD_DIR ?= ./bin
 ## Build Linux binary
 build-linux:
 	@echo "Building Linux AMD64 binary, GOFLAGS: $(GOFLAGS)"
-	@GOOS=linux GOARCH=amd64 $(GO) build -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@GOOS=linux GOARCH=amd64 $(GO) build -trimpath -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build binary
 build:
 	@echo "Building binary, GOFLAGS: $(GOFLAGS)"
-	@$(GO) build -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@$(GO) build -trimpath -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build and run binary
 run:
