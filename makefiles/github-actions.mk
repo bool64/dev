9781bfd6e3b651a5bb73294e7e3c32e43e23d245
Viacheslav Poturaev <vearutop@gmail.com>
2023-01-14 17:13:12 +0100

    Add issue templates, update bench CI task

--- makefiles/github-actions.mk
+++ makefiles/github-actions.mk
@@ -5,6 +5,7 @@ github-actions:
 	@mkdir -p $(PWD)/.github/workflows && (chmod +w $(PWD)/.github/workflows/*.yml || echo "could not chmod +w existing workflows") \
 		&& cp $(DEVGO_PATH)/templates/github/workflows/*.yml $(PWD)/.github/workflows/ \
 		&& chmod +w $(PWD)/.github/workflows/*.yml && git add $(PWD)/.github/workflows \
+		&& mkdir -p $(PWD)/.github/ISSUE_TEMPLATE && (chmod +w $(PWD)/.github/ISSUE_TEMPLATE/*.md || echo "could not chmod +w existing issue templates") \
 		&& cp $(DEVGO_PATH)/templates/github/ISSUE_TEMPLATE/*.md $(PWD)/.github/ISSUE_TEMPLATE/ \
 		&& chmod +w $(PWD)/.github/ISSUE_TEMPLATE/*.md && git add $(PWD)/.github/ISSUE_TEMPLATE \
 		&& (test -z "$(BENCH_COUNT)" && rm $(PWD)/.github/workflows/bench.yml || echo "keeping bench.yml") \
