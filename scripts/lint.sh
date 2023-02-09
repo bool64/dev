e7bf1c16fa5a9353c3cebf7927aee9cfc38c1b94
Viacheslav Poturaev <vearutop@gmail.com>
2023-02-09 14:10:07 +0100

    Bump Go and golangci-lint versions (#50)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ -z "$GO" ] && GO=go
-[ -z "$GOLANGCI_LINT_VERSION" ] && GOLANGCI_LINT_VERSION="v1.50.1"
+[ -z "$GOLANGCI_LINT_VERSION" ] && GOLANGCI_LINT_VERSION="v1.51.1"
 
 # detecting GOPATH and removing trailing "/" if any
 GOPATH="$(go env GOPATH)"
