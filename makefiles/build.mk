e1b7396b42b43dea328f42390139d2d7608d360f
Viacheslav Poturaev <vearutop@gmail.com>
2023-03-16 12:33:56 +0100

    Add control for go build flags

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -1,7 +1,10 @@
 GO ?= go
 
-# Override in app Makefile to add custom ldflags, example BUILD_LDFLAGS="-s -w"
-BUILD_LDFLAGS ?= ""
+# Override in app Makefile to add custom ldflags, example BUILD_LDFLAGS=-s -w
+BUILD_LDFLAGS ?=
+
+# Override in app Makefile to add custom build flags, example BUILD_FLAGS=-trimpath -pgo=auto
+BUILD_FLAGS ?= -trimpath
 
 # Override in app Makefile to control build target, example BUILD_PKG=./cmd/my-app
 BUILD_PKG ?= .
@@ -12,16 +15,16 @@ BUILD_DIR ?= ./bin
 ## Build Linux binary
 build-linux:
 	@echo "Building Linux AMD64 binary, GOFLAGS: $(GOFLAGS)"
-	@GOOS=linux GOARCH=amd64 $(GO) build -trimpath -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@GOOS=linux GOARCH=amd64 $(GO) build $(BUILD_FLAGS) -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build binary
 build:
 	@echo "Building binary, GOFLAGS: $(GOFLAGS)"
-	@$(GO) build -trimpath -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
+	@$(GO) build $(BUILD_FLAGS) -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" -o $(BUILD_DIR)/ $(BUILD_PKG)
 
 ## Build and run binary
 run:
 	@echo "Running binary"
-	@$(GO) run -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" $(BUILD_PKG)
+	@$(GO) run $(BUILD_FLAGS) -ldflags "$(shell bash $(DEVGO_SCRIPTS)/version-ldflags.sh && echo $(BUILD_LDFLAGS))" $(BUILD_PKG)
 
 .PHONY: build-linux build run
