54f55087abe4b05532711db5d7c108aca9524b83
Darien Hernandez <dohernandez@gmail.com>
2024-11-28 12:13:49 +0100

    feat: Turn Core←→Plug-ins
    Turn dev pkg into a Plug-ins architecture allowing to add plugins on the based targets enriching the experience of the `make` command for development.

--- makefiles/base.mk
+++ makefiles/base.mk
@@ -1,5 +1,9 @@
 #GOLANGCI_LINT_VERSION := "v1.61.0" # Optional configuration to pinpoint golangci-lint version.
 
+# MODULES is a list of dev modules (mk) to be included in the project.
+MODULES := \
+	DEVGO_PATH=github.com/bool64/dev
+
 # The head of Makefile determines location of dev-go to include standard targets.
 GO ?= go
 export GO111MODULE = on
@@ -8,24 +12,31 @@ ifneq "$(GOFLAGS)" ""
   $(info GOFLAGS: ${GOFLAGS})
 endif
 
-ifneq "$(wildcard ./vendor )" ""
-  $(info Using vendor)
-  modVendor =  -mod=vendor
+# Use vendored dependencies if available.
+ifneq ($(wildcard ./vendor),)
+  modVendor := -mod=vendor
   ifeq (,$(findstring -mod,$(GOFLAGS)))
       export GOFLAGS := ${GOFLAGS} ${modVendor}
   endif
-  ifneq "$(wildcard ./vendor/github.com/bool64/dev)" ""
-  	DEVGO_PATH := ./vendor/github.com/bool64/dev
-  endif
 endif
 
-ifeq ($(DEVGO_PATH),)
-	DEVGO_PATH := $(shell GO111MODULE=on $(GO) list ${modVendor} -f '{{.Dir}}' -m github.com/bool64/dev)
-	ifeq ($(DEVGO_PATH),)
-    	$(info Module github.com/bool64/dev not found, downloading.)
-    	DEVGO_PATH := $(shell export GO111MODULE=on && $(GO) get github.com/bool64/dev && $(GO) list -f '{{.Dir}}' -m github.com/bool64/dev)
-	endif
-endif
+# Set dev module paths or download them.
+$(foreach module,$(MODULES), \
+	$(eval key=$(word 1,$(subst =, ,$(module)))); \
+	$(eval value=$(word 2,$(subst =, ,$(module)))); \
+	\
+	$(if $(wildcard ./vendor/$(value)), \
+		$(eval export $(key)=./vendor/$(value)); \
+	) \
+	\
+	$(if $(strip $($(key))), , \
+    	$(eval export $(key)=$(shell GO111MODULE=on $(GO) list ${modVendor} -f '{{.Dir}}' -m $(value))); \
+		$(if $(strip $($(key))), \
+			$(info Module $(value) not found, downloading.); \
+			$(eval export $(key)=$(shell export GO111MODULE=on && $(GO) get $(value) && $(GO) list -f '{{.Dir}}' -m $(value))); \
+		) \
+    ) \
+)
 
 -include $(DEVGO_PATH)/makefiles/main.mk
 -include $(DEVGO_PATH)/makefiles/lint.mk
