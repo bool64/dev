c4cd649a4cadcf194ec0f1bfb1fadf4fa85c0d46
Viacheslav Poturaev <vearutop@gmail.com>
2026-03-19 20:28:16 +0100

    Update golangci-lint, use gh for release assets upload

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ -z "$GO" ] && GO=go
-[ -z "$GOLANGCI_LINT_VERSION" ] && GOLANGCI_LINT_VERSION="v2.5.0"
+[ -z "$GOLANGCI_LINT_VERSION" ] && GOLANGCI_LINT_VERSION="v2.11.3"
 
 # detecting GOPATH and removing trailing "/" if any
 GOPATH="$(go env GOPATH)"
